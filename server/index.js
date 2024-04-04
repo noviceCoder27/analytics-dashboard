7b7970c63051656c6e97fc76424db847d35a6c41
mdGoswami <u19cse1001@cit.ac.in>
2024-04-04 19:58:09 +0530

    refactor

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
     origin: [`${process.env.CLIENT_URL}`],
-    methods: ["GET","POST","PATCH", "DELETE"],
+    methods: ["GET"],
     credentials: true
 }));
 app.use(express.json());
