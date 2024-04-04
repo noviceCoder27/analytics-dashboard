b866cd50f1d89dabfcecc5dd0bfe2b8d082a378c
mdGoswami <u19cse1001@cit.ac.in>
2024-04-04 19:48:32 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: [`${process.env.CLIENT_URL}`],
+    origin: ["https://analytics-dashboard-six-sand.vercel.app"],
     methods: ["GET"],
     credentials: true
 }));
